d112c8b9e45eeafb775dfc0790094bbe30f24946
siemenvdv <siemen.vandervoort@student.uhasselt.be>
2022-01-03 16:06:12 +0100

    Project upload

--- App.py
+++ App.py
@@ -15,7 +15,6 @@ cred_obj = firebase_admin.credentials.Certificate('weatherstation-iot-8558e-fire
 databaseURL = "https://weatherstation-iot-8558e-default-rtdb.firebaseio.com/"
 default_app = firebase_admin.initialize_app(cred_obj, {'databaseURL':databaseURL})
 from firebase_admin import db
-ref = db.reference("/Data")
 
 eventlet.monkey_patch()
 
@@ -23,8 +22,6 @@ app = Flask(__name__)
 app.config['SECRET'] = 'my secret key'
 app.config['TEMPLATES_AUTO_RELOAD'] = False
 app.config['MQTT_BROKER_URL'] = 'e83b7b9f6de84440b629c443336adf69.s1.eu.hivemq.cloud'
-#app.config['MQTT_BROKER_URL'] = '192.168.1.5'
-#app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = 'psoc6_1'
 app.config['MQTT_PASSWORD'] = 'IoT_4482'
 app.config['MQTT_KEEPALIVE'] = 5
@@ -33,35 +30,21 @@ app.config['MQTT_KEEPALIVE'] = 5
 app.config['MQTT_BROKER_PORT'] = 8883
 app.config['MQTT_TLS_ENABLED'] = True
 app.config['MQTT_TLS_INSECURE'] = False
-#app.config['MQTT_TLS_CA_CERTS'] = 'server.pem'
 app.config['MQTT_TLS_CA_CERTS'] = 'cacert.pem'
-#app.config['MQTT_TLS_CERTFILE'] = 'client.crt'
-#app.config['MQTT_TLS_KEYFILE'] = 'client.key'
 app.config['CLIENT_ID'] = 'Rocky'
 
 app.config['MQTT_TLS_VERSION'] = ssl.PROTOCOL_TLSv1_2
 
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 mqtt = Mqtt(app)
-#socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
 socketio = SocketIO(app,cors_allowed_origins='*')
-#socketio.init_app(app, cors_allowed_origins="*")
-
-
-@app.route('/dashboard')
-def index_dashboard():
-    mqtt.subscribe('ledstatus')
-    return render_template('dashboard.html')
-
-@app.route('/dashboard2')
-def index_dashboard2():
-    mqtt.subscribe('ledstatus')
-    return render_template('test.html')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	mqtt.subscribe('ledstatus')
+	mqtt.subscribe('moist')
+	return render_template('dashboard.html')
 
 @socketio.on('publish')
 def handle_publish(json_str):
@@ -98,25 +81,41 @@ def handle_mqtt_message(client, userdata, message):
 			timeList[value] = int(string[1:2])
 	print(str(timeList))
 	"""
+	print(str(data))
 
-	print("Time: " + str(timeList) + ",	Data: " + str(data["payload"]))
+	#Upload temperature data
+	if data["topic"] == "ledstatus":
+		ref = db.reference("/Temperature")
+		print("Time: " + str(timeList) + ",	Temp: " + str(data["payload"]))
 
-	json_date = "\"date\": " + str((timeList))
-	json_data = "\"data\": " + str(data["payload"])
+		json_date = "\"date\": " + str((timeList))
+		json_data = "\"data\": " + str(data["payload"])
 
-	json_string = str("{" + json_date + ", " + json_data + "}")
+		json_string = str("{" + json_date + ", " + json_data + "}")
 
-	ref.push().set(json_string)
+		ref.push().set(json_string) #Enable!!
+
+	#Upload moist data
+	elif data["topic"] == "moist":
+		ref = db.reference("/Moist")
+		print("Time: " + str(timeList) + ",	Moist: " + str(data["payload"]))
+
+		json_date = "\"date\": " + str((timeList))
+		json_data = "\"data\": " + str(data["payload"])
+
+		json_string = str("{" + json_date + ", " + json_data + "}")
+
+		ref.push().set(json_string) #Enable!!
+	else:
+		print("Unknown topic!")
 
 	socketio.emit('mqtt_message', data=data)
 
-@app.route('/returnData')
-def returnData():
-	print("Displaying historical data")
-
 @app.route('/data_today')
 def data_today():
-	#Get the data
+	#Get the temperature data
+	print("Getting temperature history data ...")
+	ref = db.reference("/Temperature")
 	data = ref.get()
 	data_json = json.dumps(data)
 	data_json = json.loads(data_json)
@@ -141,11 +140,39 @@ def data_today():
 			)
 			socketio.emit('historical_data', data=data)
 
+	#Get the moist data
+	print("Getting moist history data ...")
+	ref = db.reference("/Moist")
+	data = ref.get()
+	data_json = json.dumps(data)
+	data_json = json.loads(data_json)
+
+	#Get the values
+	time = []
+	value = []
+	for dataPoint in data_json.values():
+		json_string = json.loads(dataPoint)
+		time.append(json_string["date"])
+		value.append(json_string["data"])
+
+	#Get data of today
+	day = time[len(time)-1][0]
+	times = []
+	values = []
+	for counter in range(0, len(value)):
+		if time[counter][0] == day:
+			data = dict(
+				time = time[counter][3:6],
+				value = value[counter]
+			)
+			socketio.emit('moist_Historical_Data', data=data)
+
 	return ("Not needed")
 
 @app.route('/data_yesterday')
 def data_yesterday():
-	#Get the data
+	#Get the temperature data
+	ref = db.reference("/Temperature")
 	data = ref.get()
 	data_json = json.dumps(data)
 	data_json = json.loads(data_json)
@@ -168,6 +195,30 @@ def data_yesterday():
 			)
 			socketio.emit('historical_data', data=data)
 
+	#Get the moist data
+	ref = db.reference("/Moist")
+	data = ref.get()
+	data_json = json.dumps(data)
+	data_json = json.loads(data_json)
+
+	#Get the values
+	time = []
+	value = []
+	for dataPoint in data_json.values():
+		json_string = json.loads(dataPoint)
+		time.append(json_string["date"])
+		value.append(json_string["data"])
+
+	#Get data of yesterday
+	day = time[len(time)-1][0] - 1
+	for counter in range(0, len(value)):
+		if time[counter][0] == day:
+			data = dict(
+				time = time[counter][3:6],
+				value = value[counter]
+			)
+			socketio.emit('moist_Historical_Data', data=data)
+
 	return ("Not needed")
 
 @app.route('/calendarInput', methods=['POST'])
@@ -175,7 +226,8 @@ def calendarInput():
 	date = request.form['date']
 	print("Choosen date: " + str(date))
 
-	#Get the data
+	#Get the temperature data
+	ref = db.reference("/Temperature")
 	data = ref.get()
 	data_json = json.dumps(data)
 	data_json = json.loads(data_json)
@@ -192,13 +244,63 @@ def calendarInput():
 	year = str(date[0:4])
 	month = str(date[5:7])
 	day = str(date[8:10])
+
+	if int(day) < 10:
+		day = str(day[1])
+	if int(month) < 10:
+		month = str(month[1])
+
+	times = []
+	values = []
 	for counter in range(0, len(value)):
 		if str(time[counter][0]) == day and str(time[counter][1]) == month and str(time[counter][2]) == year:
-			data = dict(
-				time = time[counter][3:6],
-				value = value[counter]
-			)
-			socketio.emit('historical_data', data=data)
+			values.append(value[counter])
+			times.append(time[counter][3:6])
+
+	data = dict(
+		time = times,
+		value = values
+	)
+
+	socketio.emit('historical_data_cal', data=data)
+
+	#Get the moist data
+	ref = db.reference("/Moist")
+	data = ref.get()
+	data_json = json.dumps(data)
+	data_json = json.loads(data_json)
+
+	#Get the values
+	time = []
+	value = []
+	for dataPoint in data_json.values():
+		json_string = json.loads(dataPoint)
+		time.append(json_string["date"])
+		value.append(json_string["data"])
+
+	#Get data of input date
+	year = str(date[0:4])
+	month = str(date[5:7])
+	day = str(date[8:10])
+
+	if int(day) < 10:
+		day = str(day[1])
+	if int(month) < 10:
+		month = str(month[1])
+
+	times = []
+	values = []
+	for counter in range(0, len(value)):
+		if str(time[counter][0]) == day and str(time[counter][1]) == month and str(time[counter][2]) == year:
+			values.append(value[counter])
+			times.append(time[counter][3:6])
+
+	data = dict(
+		time = times,
+		value = values
+	)
+
+	socketio.emit('moist_Historical_Data_cal', data=data)
 
 	return ('', 204)
 
